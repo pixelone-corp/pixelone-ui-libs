44d3a89a77073129ca16f65191f5ff81d5364a11
saif <saifrehman9796@gmail.com>
2022-06-17 15:45:15 +0500

    FIX

--- src/lib/pixel-input/components/TypeAHead.tsx
+++ src/lib/pixel-input/components/TypeAHead.tsx
@@ -1,36 +1,36 @@
-import React from "react";
-import { AsyncTypeahead } from "react-bootstrap-typeahead";
-import "react-bootstrap-typeahead/css/Typeahead.css";
+import React from 'react'
+import { AsyncTypeahead } from 'react-bootstrap-typeahead'
+import 'react-bootstrap-typeahead/css/Typeahead.css'
 
 const TypeAHead = (props) => {
-  const { handleSearch, isLoading, data, labelKey } = props;
-  const filterBy = () => true;
-  const [options, setOptions] = React.useState(data);
-  const ref = React.createRef();
+  const { handleSearch, isLoading, data, labelKey } = props
+  const filterBy = () => true
+  const [options, setOptions] = React.useState(data)
+  const ref = React.useRef()
 
   React.useEffect(() => {
-    console.log(data);
+    console.log(data)
 
-    setOptions(data);
-  }, [data]);
+    setOptions(data)
+  }, [data])
 
   return (
     <AsyncTypeahead
       filterBy={filterBy}
-      id="async-example"
+      id='async-example'
       ref={ref}
-      labelKey={labelKey || "first_name"}
+      labelKey={labelKey || 'first_name'}
       isLoading={isLoading}
       onSearch={handleSearch}
       options={options}
       placeholder={props.placeholder}
       renderMenuItemChildren={props.formatter}
       onChange={(d) => {
-        props.onChange(d);
-        ref.current.clear();
+        props.onChange(d)
+        ref.current.clear()
       }}
     />
-  );
-};
+  )
+}
 
-export default TypeAHead;
+export default TypeAHead
